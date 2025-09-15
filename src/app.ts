925410576f62fa025d0408f1038b340c68c3d18b
Md Raiyan siddik <raiyansiddik2022@gmail.com>
2025-09-16 02:21:15 +0600

    simple

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,11 @@
 import "dotenv/config";
 import express from "express";
-import cors from "cors";
-import { createServer } from "http";
 import cookieParser from "cookie-parser";
-import { connectDB } from "@/config/db.config";
 import serverless from "serverless-http";
 const app = express();
-const server = createServer(app);
 app.set("trust proxy", true);
 app.use(cookieParser());
 app.use(express.json());
-// app.use("/api", (await import("@/routes/vendor.route")).default);
-
-connectDB();
-
 app.get("/", async (req, res) => {
   res.json({
     success: true,
@@ -21,9 +13,5 @@ app.get("/", async (req, res) => {
     publicIP: req.ip,
   });
 });
-
-// server.listen(process.env.PORT, () =>
-//   console.log(`Server running on port ${process.env.PORT}`)
-// );
 
 export const handler = serverless(app);
